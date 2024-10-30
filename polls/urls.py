0f7194c80a5333956aeb0773fa125629adbf78f0
Harry Best <harry.best@softwire.com>
2024-10-30 14:26:42 +0000

    basic site

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.urls import path
+
+from . import views
+
+app_name = "polls"
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("<int:question_id>/success/", views.success, name="success"),
+]
